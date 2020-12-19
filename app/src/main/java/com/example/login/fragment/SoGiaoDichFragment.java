2f7ce65473da24880de8a40494b2ef2951c2be82
Nghia <nghia02584@gmail.com>
2020-12-19 13:58:05 +0700

    Cập nhật thanh bar tổng trên top

--- app/src/main/java/com/example/login/fragment/SoGiaoDichFragment.java
+++ app/src/main/java/com/example/login/fragment/SoGiaoDichFragment.java
@@ -2,6 +2,7 @@ package com.example.login.fragment;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -57,10 +58,5 @@ public class SoGiaoDichFragment extends Fragment {
         }
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_so_giao_dich, container, false);
-    }
+
 }
