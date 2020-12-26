af6032155e9e0c94e22af15ca3fa36a7c565849a
tannghia025 <55869601+tannghia025@users.noreply.github.com>
2020-12-26 20:10:05 +0700

    Update BaoCaoFragment.java

--- app/src/main/java/com/example/login/fragment/BaoCaoFragment.java
+++ app/src/main/java/com/example/login/fragment/BaoCaoFragment.java
@@ -61,6 +61,7 @@ public class BaoCaoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        return bayba tum lum cho looix choiw hy hy
         return inflater.inflate(R.layout.fragment_bao_cao, container, false);
     }
 }
